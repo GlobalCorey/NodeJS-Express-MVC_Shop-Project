c4d99321e25595c9fe0f36414db4caa74777a55e
GlobalCorey <corey4994@gmail.com>
2019-10-23 18:03:05 -0500

    Finished NodeJS project. Prepped for Heroku deployment

---
+++ test/auth-middleware.js
@@ -0,0 +1,11 @@
+const authMiddleware = require('../middleware/is-auth');
+
+it('should throw an error if no authorization header is present', function(){
+    const req = {
+        get: function() {
+            return null;
+        }
+    };
+
+    authMiddleware(req)
+})
